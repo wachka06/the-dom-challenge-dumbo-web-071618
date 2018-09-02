b5beb738bf4723cfda92022bc34b2e34c3973cdd
wachka06 <wachka06@gmail.com>
2018-09-02 18:42:43 -0400

    done

--- challenge.js
+++ challenge.js
@@ -49,24 +49,24 @@ function minus(){
 //      ul.appendChild(li);
 //  }
 
-// var count = {};
+
 
  function display(count){
    const likes = document.querySelector('.likes');
-   likes.innerHTML = ""
+   likes.innerHTML = ""; // This line clear the content of innerHTML after each iteration.
 
 
    for (const timer in count){
     let li = document.createElement('li');
-    li.innerHTML = `${timer} has been liked ${count[timer]} times`;
+    li.innerHTML = `${timer} has been <strong>liked</strong> ${count[timer]} times`;
     likes.appendChild(li)
    }
-   // likes;
+   // When you use "appendChild," (1)add the text in innerHTML, (2)use "appendChild."
  }
 
 
   function likes(){
-    const count = {};
+    const count = {}; // We need to make a countainer to save the information of (1)the number of setTimeout and (2)the number of how many times the user clicked "likes". 
     document.getElementById('<3').addEventListener('click', function(){
       let timer = document.getElementById('counter').innerHTML;
       if(count[timer] == undefined) {
@@ -74,8 +74,7 @@ function minus(){
       } else {
         count[timer] += 1
       }
-      display(count);
-      // console.log(count);
+      display(count); //You don't need to return, but if you do, you need to return in the "addEventListener."
     })
   }
 
@@ -83,4 +82,4 @@ function minus(){
 
 // display()
 
-likes()
+likes() // You have to call function!
